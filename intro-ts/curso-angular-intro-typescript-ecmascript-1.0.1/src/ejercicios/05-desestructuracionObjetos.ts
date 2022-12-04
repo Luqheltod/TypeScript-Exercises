a95765d6fb3024a069d608a179eeb41d7840fdf9
German Sanchez Pastor <luqh555@gmail.com>
2022-12-04 12:55:14 +0100

    ejercicio 5

---
+++ intro-ts/curso-angular-intro-typescript-ecmascript-1.0.1/src/ejercicios/05-desestructuracionObjetos.ts
@@ -0,0 +1,30 @@
+
+
+interface Reproductor{
+    volumen : number;
+    segundo : number;
+    cancion : string;
+    detalles : Detalles
+}
+
+interface Detalles{
+    autor : string;
+    anio : number
+}
+
+const reproductor : Reproductor = {
+    volumen : 90,
+    segundo : 36,
+    cancion : "Mess",
+    detalles : {
+        autor : "Ed Sheeran",
+        anio: 2015
+    }
+}
+
+
+const {volumen , segundo, cancion, detalles} = reproductor;
+const {autor } = detalles;
+
+/* Tambien se puede desestructurar asi */
+//const {volumen , segundo, cancion, detalles{ autor }} = reproductor;
